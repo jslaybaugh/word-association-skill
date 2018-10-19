d5aedfff17f3b704f60c975320c442eca647b93d
Jorin Slaybaugh <jslaybaugh@gmail.com>
2018-10-18 22:10:06 -0500

    finished?

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const request = require('request-promise-native');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
-        console.log(handlerInput.requestEnvelope.request);
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
-    handle(handlerInput) {
-        const speechText = 'Welcome to Word Association! Start by saying any word and I\'ll respond with the first thing that comes to mind and then we\'ll go back and forth from there! What word should we start with?';
-        return handlerInput.responseBuilder
+    async handle(handlerInput) {
+        const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
+        const speechText = 'Welcome to Word Association! When you say a word, I\'ll respond with the first thing that comes to mind and then we\'ll go back and forth from there! Remember! Stop, cancel, exit, and help are special words, so only use them when you want to stop or when you need help.<break time="1s"></break>You go first!! Start by saying "Let\'s start with BLANK" or "<phoneme alphabet="ipa" ph="əˈsoʊʃieɪt">associate</phoneme> BLANK".';
+        return responseBuilder
             .speak(speechText)
             .reprompt(speechText)
-            //TODO .withSimpleCard('Hello World', speechText)
+            .withSimpleCard('Welcome', "Welcome to Word Association!")
             .getResponse();
     }
 };
@@ -21,69 +21,62 @@ const WordAssociationIntentHandler = {
     canHandle(handlerInput) {
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'WordAssociationIntent'
-            && handlerInput.requestEnvelope.request.dialogState !== 'COMPLETED';;
+            && handlerInput.requestEnvelope.request.dialogState !== 'COMPLETED';
     },
     async handle(handlerInput) {
         const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
-        console.log(requestEnvelope.request);
         const { intent } = requestEnvelope.request;
-        console.log("hi");
+        const word = intent.slots.word.value;
 
-        const errorResponses = [
-            "Hmm. I'm having a hard time thinking of something for that one. Can you give me another word?",
-            "Sorry, I got nothing... can we try a different word?",
-            "Uh oh. I swear this never happens. Please repeat that for me?"
-        ];
-        const prefixes = [
-            { text: "", weight: 7 },
-            { text: "That reminds me of... ", weight: 1 },
-            { text: "How about... ", weight: 1 },
-            { text: "Oooh, good one! Let's go with... ", weight: 1 }
-        ];
-        const threshhold = 75;
-        const pos = "noun,adjective,verb,adverb";
+        if (!word || word.length < 1)
+            return ErrorHandler();
+        if (word && (word.toLowerCase() == "cancel" || word.toLowerCase() == "stop"))
+            return CancelAndStopIntentHandler.handle(handlerInput);
+        if (word && (word.toLowerCase() == "help"))
+            return HelpIntentHandler.handle(handlerInput);
 
-
-
-        let weightTotal = 0;
-        let randomPrefixes = [];
-        for (var i = 0; i < prefixes.length; i++)
+        // keep track of the words in session
+        let words = [];
+        let sessionAttributes = attributesManager.getSessionAttributes();
+        if (sessionAttributes && sessionAttributes.words)
         {
-            for (var j = 0; j < prefixes[i].weight; j++)
-            {
-                randomPrefixes.push(prefixes[i].text);
-                weightTotal += 1;
-            }
+            words = sessionAttributes.words;
         }
+        words.push(word.toUpperCase());
 
-        const errorResponse = errorResponses[Math.round(Math.random() * (errorResponses.length-1))];
-        const randomPrefix = randomPrefixes[Math.round(Math.random() * (weightTotal-1))];
+        // start to formulate flourishes for responses
+        const errorResponse = Constants.ERROR_RESPONSES[Math.round(Math.random() * (Constants.ERROR_RESPONSES.length-1))];
+        const randomPrefix = Constants.PREFIXES[Math.round(Math.random() * (Constants.PREFIXES.length-1))];
 
 
         let wordResponse = null;
         let isError = false;
-        const word = intent.slots.word.value;
         console.log(word);
-        var wordAssociationApiParams = {
+        const API_PARAMS = {
             resolveWithFullResponse: true,
-            url: "https://api.wordassociations.net/associations/v1.0/json/search?apikey=&text=" + encodeURIComponent(word) + "&lang=en&type=response&indent=no&pos=" + pos,
+            url: "https://api.wordassociations.net/associations/v1.0/json/search?apikey=&lang=en&type=response&indent=no&pos=noun,adjective,verb,adverb&text=" + encodeURIComponent(word),
             json: true
-          };
-          await request(wordAssociationApiParams)
+        };
+          await request(API_PARAMS)
             .then(function(response) {
-              if (response.statusCode == 200
-                && response
+              if (response
+                && response.statusCode == 200
                 && response.body
                 && response.body.response
                 && response.body.response[0]
-                && response.body.response[0].items)
+                && response.body.response[0].items
+                && response.body.response[0].items[0])
                 {
                     let items = response.body.response[0].items;
                     
                     let threshholdItems = [];
                     for (var i = 0; i < items.length; i++)
                     {
-                        if (items[i].weight >= threshhold)
+                        if (items[i] 
+                            && items[i].weight 
+                            && items[i].weight >= Constants.THRESHHOLD 
+                            && items[i].item 
+                            && words.indexOf(items[i].item.toUpperCase()) < 0)
                         {
                             threshholdItems.push(items[i]);
                         }
@@ -96,24 +89,32 @@ const WordAssociationIntentHandler = {
                         && threshholdItems[0]
                         && threshholdItems[random]
                         && threshholdItems[random].item)
+                     {
                         wordResponse = threshholdItems[random].item;
+                        words.push(wordResponse.toUpperCase());
+                    }   
                 }
 
               else {
                 console.log('response: ', response.statusCode, response.body);
                 wordResponse = errorResponse;
+                words.pop();
                 isError = true;
               }
             })
             .catch(function(err) {
-                console.log('response: ', err);
+                console.log('error: ', err);
                 wordResponse = errorResponse;
+                words.pop();
                 isError = true;
             });
 
+            sessionAttributes.words = words;
+            attributesManager.setSessionAttributes(sessionAttributes);
+
             return responseBuilder
                 .speak((isError ? "" : randomPrefix) + wordResponse)
-                .withSimpleCard('Word Association', wordResponse)
+                .withSimpleCard(wordResponse.toUpperCase(), (isError ? "" : randomPrefix) + wordResponse)
                 .addElicitSlotDirective("word")
                 .getResponse();
         
@@ -126,11 +127,11 @@ const HelpIntentHandler = {
             && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speechText = 'You can say hello to me!';
+        const speechText = 'You can say any word and I\'ll attempt to respond to it with a word it reminds me of. Or you can say stop, cancel, or exit at any time.';
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(speechText)
-            .withSimpleCard('Hello World', speechText)
+            .withSimpleCard('Help', speechText)
             .getResponse();
     }
 };
@@ -142,10 +143,22 @@ const CancelAndStopIntentHandler = {
                 || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
-        const speechText = 'Goodbye!';
+        const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
+        let extra = "See you next time!";
+        console.log(attributesManager.getSessionAttributes())
+        if (attributesManager)
+        {
+            let sessionAttributes = attributesManager.getSessionAttributes();
+            if (sessionAttributes && sessionAttributes.words && sessionAttributes.words[0])
+            {
+                extra = "We went from " + sessionAttributes.words[0] + " to " + sessionAttributes.words[sessionAttributes.words.length-1] + " in " + sessionAttributes.words.length + " total word" + (sessionAttributes.words.length == 1 ? "" : "s") + "! " + extra;
+            }
+        }
+        const speechText = "That was fun! " + extra;
+       
         return handlerInput.responseBuilder
             .speak(speechText)
-            .withSimpleCard('Hello World', speechText)
+            .withSimpleCard('Goodbye', speechText)
             .getResponse();
     }
 };
@@ -156,7 +169,20 @@ const SessionEndedRequestHandler = {
     },
     handle(handlerInput) {
         //any cleanup logic goes here
-        return handlerInput.responseBuilder.getResponse();
+        const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
+        let extra = "See you next time!";
+        if (attributesManager)
+        {
+            let sessionAttributes = attributesManager.getSessionAttributes();
+            if (sessionAttributes && sessionAttributes.words && sessionAttributes.words[0])
+            {
+                extra = "We went from " + sessionAttributes.words[0] + " to " + sessionAttributes.words[sessionAttributes.words.length-1] + " in " + sessionAttributes.words.length + " total word" + (sessionAttributes.words.length == 1 ? "" : "s") + "! " + extra;
+            }
+        }
+        const speechText = "That was fun! " + extra;
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('Goodbye', speechText);
     }
 };
 
@@ -171,6 +197,27 @@ const ErrorHandler = {
         .reprompt('Sorry, I can\'t understand the command. Please say again.')
         .getResponse();
     },
+};
+
+const Constants = {
+    ERROR_RESPONSES:[
+        "Hmm. I'm having a hard time thinking of something for that one. Can you give me another word?",
+        "Sorry, I got nothing... can we try a different word?",
+        "Uh oh. Something went wrong. Please repeat that for me?"
+    ],
+    PREFIXES: [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "That reminds me of... ",
+        "That reminds me of... ",
+        "How about... ",
+        "How about... ",
+        "Oooh, good one! Let's go with... "
+    ],
+    THRESHHOLD: 75
 };
 
 
