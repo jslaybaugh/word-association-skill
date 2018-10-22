6380132273ba1bff69a914e91dfcec7ab673677b
Jorin Slaybaugh <jslaybaugh@gmail.com>
2018-10-21 22:13:23 -0500

    resubmit

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const request = require('request-promise-native');
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
+        console.log('LaunchRequestHandler')
         return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
     },
     async handle(handlerInput) {
+        console.log('LaunchRequestHandler handle');
         const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
-        const speechText = 'Welcome to Word Association! When you say a word, I\'ll respond with the first thing that comes to mind and then we\'ll go back and forth from there! Remember! Stop, cancel, exit, and help are special words, so only use them when you want to stop or when you need help.<break time="1s"></break>You go first!! Start by saying "Let\'s start with BLANK" or "<phoneme alphabet="ipa" ph="əˈsoʊʃieɪt">associate</phoneme> BLANK".';
+        const speechText = 'Welcome to Word Association! When you say a word, I\'ll respond with the first thing that comes to mind and then we\'ll go back and forth from there! Remember! Stop, cancel, exit, and help are special words, so only use them when you want to stop or when you need help.<break time="500ms"></break>You go first!! Start by saying "Let\'s start with BLANK" or "<phoneme alphabet="ipa" ph="əˈsoʊʃieɪt">associate</phoneme> BLANK".';
         return responseBuilder
             .speak(speechText)
             .reprompt(speechText)
@@ -19,21 +21,30 @@ const LaunchRequestHandler = {
 
 const WordAssociationIntentHandler = {
     canHandle(handlerInput) {
+        console.log('WordAssociationIntentHandler');
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'WordAssociationIntent'
             && handlerInput.requestEnvelope.request.dialogState !== 'COMPLETED';
     },
     async handle(handlerInput) {
+        console.log('WordAssociationIntentHandler handle');
         const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
         const { intent } = requestEnvelope.request;
-        const word = intent.slots.word.value;
+        let word = intent.slots.word.value;
 
         if (!word || word.length < 1)
             return ErrorHandler();
         if (word && (word.toLowerCase() == "cancel" || word.toLowerCase() == "stop"))
             return CancelAndStopIntentHandler.handle(handlerInput);
+        if (word && (word.toLowerCase() == "exit"))
+            return SessionEndedRequestHandler.handle(handlerInput);
         if (word && (word.toLowerCase() == "help"))
             return HelpIntentHandler.handle(handlerInput);
+        if (word && (/repeat/ig.test(word)))
+            return RepeatIntentHandler.handle(handlerInput);
+
+
+        word = word.replace(/(let's start with|I'll start with|let's go with|how about|that reminds me of|associate)\s+/ig,"");
 
         // keep track of the words in session
         let words = [];
@@ -93,6 +104,12 @@ const WordAssociationIntentHandler = {
                         wordResponse = threshholdItems[random].item;
                         words.push(wordResponse.toUpperCase());
                     }   
+                    else
+                    {
+                        wordResponse = errorResponse;
+                        words.pop();
+                        isError = true;
+                    }
                 }
 
               else {
@@ -123,11 +140,14 @@ const WordAssociationIntentHandler = {
 
 const HelpIntentHandler = {
     canHandle(handlerInput) {
+        console.log('HelpIntentHandler')
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speechText = 'You can say any word and I\'ll attempt to respond to it with a word it reminds me of. Or you can say stop, cancel, or exit at any time.';
+        console.log('HelpIntentHandler handle')
+        const speechText = 'Sounds like you\'re asking for help... You can say any word and I\'ll attempt to respond to it with a word it reminds me of. Or you can say stop, cancel, or exit at any time. Start by saying "<phoneme alphabet="ipa" ph="əˈsoʊʃieɪt">associate</phoneme> BLANK".';
+
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(speechText)
@@ -136,25 +156,56 @@ const HelpIntentHandler = {
     }
 };
 
+const RepeatIntentHandler = {
+    canHandle(handlerInput) {
+        console.log('RepeatIntentHandler')
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.RepeatIntent';
+    },
+    handle(handlerInput) {
+        console.log('RepeatIntentHandler handle')
+        const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
+        const { intent } = requestEnvelope.request;
+
+        let speechText = "";
+        let sessionAttributes = attributesManager.getSessionAttributes();
+        if (sessionAttributes && sessionAttributes.words && sessionAttributes.words[0])
+        {
+            speechText = sessionAttributes.words[sessionAttributes.words.length-1];
+        }
+
+        return responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Repeated', speechText)
+            .addElicitSlotDirective("word")
+            .getResponse();
+    }
+};
+
 const CancelAndStopIntentHandler = {
     canHandle(handlerInput) {
+        console.log('CancelAndStopIntentHandler')
         return handlerInput.requestEnvelope.request.type === 'IntentRequest'
             && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
                 || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
     },
     handle(handlerInput) {
+        console.log('CancelAndStopIntentHandler handle')
         const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
         let extra = "See you next time!";
+        let extraAdded = false;
         console.log(attributesManager.getSessionAttributes())
         if (attributesManager)
         {
             let sessionAttributes = attributesManager.getSessionAttributes();
             if (sessionAttributes && sessionAttributes.words && sessionAttributes.words[0])
             {
+                extraAdded = true;
                 extra = "We went from " + sessionAttributes.words[0] + " to " + sessionAttributes.words[sessionAttributes.words.length-1] + " in " + sessionAttributes.words.length + " total word" + (sessionAttributes.words.length == 1 ? "" : "s") + "! " + extra;
             }
         }
-        const speechText = "That was fun! " + extra;
+        let speechText = "Sounds like you want to stop" + (extraAdded ? "... but that was fun! " : ". ") + extra;
        
         return handlerInput.responseBuilder
             .speak(speechText)
@@ -163,11 +214,32 @@ const CancelAndStopIntentHandler = {
     }
 };
 
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        console.log('FallbackIntentHandler')
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        console.log('FallbackIntentHandler handle')
+        const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
+        
+        // const speechText = "We're already playing. Say any word and we'll continue."
+        // return responseBuilder
+        //     .speak(speechText)
+        //     .withSimpleCard('Fallback', speechText)
+        //     .getResponse();
+        return LaunchRequestHandler.handle(handlerInput);
+    }
+};
+
 const SessionEndedRequestHandler = {
     canHandle(handlerInput) {
+        console.log('SessionEndedRequestHandler')
         return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
     },
     handle(handlerInput) {
+        console.log('SessionEndedRequestHandler handle')
         //any cleanup logic goes here
         const { requestEnvelope, attributesManager, responseBuilder } = handlerInput;
         let extra = "See you next time!";
@@ -188,9 +260,11 @@ const SessionEndedRequestHandler = {
 
 const ErrorHandler = {
     canHandle() {
+        console.log('ErrorHandler')
       return true;
     },
     handle(handlerInput, error) {
+        console.log('ErrorHandler handle')
       console.log(`Error handled: ${error.message}`);
     return handlerInput.responseBuilder
         .speak('Sorry, I can\'t understand the command. Please say again.')
@@ -222,9 +296,12 @@ const Constants = {
 
 
 exports.handler = Alexa.SkillBuilders.custom()
-     .addRequestHandlers(LaunchRequestHandler,
-                         WordAssociationIntentHandler,
-                         HelpIntentHandler,
+     .addRequestHandlers(HelpIntentHandler,
                          CancelAndStopIntentHandler,
-                         SessionEndedRequestHandler)
+                         SessionEndedRequestHandler,
+                         FallbackIntentHandler,
+                         RepeatIntentHandler,
+                         WordAssociationIntentHandler,
+                         LaunchRequestHandler)
+     .addErrorHandlers(ErrorHandler)
      .lambda();
